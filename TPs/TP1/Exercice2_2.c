21380da3f1e224b9c264485fa5cec728f7b10454
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-01-26 21:28:38 +0100

    Update to fit conventions

--- TPs/TP1/Exercice2_2.c
+++ TPs/TP1/Exercice2_2.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned fact(unsigned n)
-{
-    if (n == 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return n * fact(n - 1);
-    }
-}
+unsigned fact(unsigned n);
 
 int main()
 {
@@ -23,4 +13,16 @@ int main()
     printf("Factorial of %u: %u\n", n, fact(n));
 
     return EXIT_SUCCESS;
+}
+
+unsigned fact(unsigned n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
 }
--- TPs/TP1/Exercice2_3.c
+++ TPs/TP1/Exercice2_3.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fact(unsigned n)
-{
-    unsigned acc = 1;
-
-    for (unsigned i = 1; i <= n; ++i)
-    {
-        acc *= i;
-        printf("%u! = %u\n", i, acc);
-    }
-}
+void fact(unsigned n);
 
 int main()
 {
@@ -22,4 +13,14 @@ int main()
     fact(n);
 
     return EXIT_SUCCESS;
+}
+void fact(unsigned n)
+{
+    unsigned acc = 1;
+
+    for (unsigned i = 1; i <= n; ++i)
+    {
+        acc *= i;
+        printf("%u! = %u\n", i, acc);
+    }
 }
