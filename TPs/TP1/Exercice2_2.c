53a64da0195b299ebcacc9280d848efe4fac2068
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-01-26 20:43:05 +0100

    exo 2.2

---
+++ TPs/TP1/Exercice2_2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned fact(unsigned n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
+
+int main()
+{
+    unsigned n;
+
+    printf("Enter a number: ");
+    scanf("%u", &n);
+
+    printf("Factorial of %u: %u\n", n, fact(n));
+
+    return EXIT_SUCCESS;
+}
