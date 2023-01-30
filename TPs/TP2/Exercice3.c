01bf63337e03ed56aee65e1e6afb6dab7c364d50
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-01-30 19:17:29 +0100

    Exo 3

---
+++ TPs/TP2/Exercice3.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+unsigned f(unsigned n);
+
+unsigned g(unsigned n);
+
+int main(int argc, char const *argv[])
+{
+
+    printf("f(0) : %d\n", f(0));
+    printf("f(1) : %d\n", f(1));
+    printf("f(20) : %d\n", f(20));
+
+    return 0;
+}
+
+unsigned f(unsigned n)
+{
+    if (n <= 1)
+    {
+        return 2;
+    }
+    else
+    {
+        return 2 * g(n - 1);
+    }
+}
+
+unsigned g(unsigned n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 3 * f(n / 2);
+    }
+}
