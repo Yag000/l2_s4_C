92e53f40a068ee785af3270cdcd5bca5b02860bb
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-03-17 09:18:02 +0100

    Exo 1.2 not working

--- TPs/TP7/Exercice1.c
+++ TPs/TP7/Exercice1.c
@@ -17,7 +17,6 @@ void test_next_word();
 
 int main()
 {
-
     test_nbr_words();
     test_word_len();
     test_extract_word();
@@ -25,7 +24,6 @@ int main()
 }
 int nbr_words(const char *s)
 {
-
     int words_found = 0;
     bool is_inside_word = false;
 
@@ -50,9 +48,22 @@ int nbr_words(const char *s)
     return words_found;
 }
 
+int word_len(const char *w)
+{
+    assert(!isspace(w));
+
+    int word_length = 0;
+    while (!isspace(w) && *w)
+    {
+        word_length++;
+        w++;
+    }
+
+    return word_length;
+}
+
 void test_nbr_words()
 {
-
     puts("---------------- Test nbr_words ----------------");
 
     char *s1 = "Hello world!";
@@ -72,7 +83,32 @@ void test_nbr_words()
 
     char *s6 = " a aa ba a bbbb ";
     printf("%s : %d, expected 5\n", s6, nbr_words(s6));
+
+    puts("---------------- End Test nbr_words ----------------\n\n");
 }
-void test_word_len() {}
+void test_word_len()
+{
+    puts("---------------- Test word_len ----------------");
+
+    char *s1 = "Hello world!";
+    printf("%s : %d, expected 5\n", s1, word_len(s1));
+
+    char *s2 = "world!";
+    printf("%s : %d, expected 6\n", s2, word_len(s2));
+
+    char *s3 = "\t\tHello world!  ";
+    printf("%s adr = s + 2: %d, expected 4\n", s3, word_len(s3 + 2));
+
+    char *s4 = "   Hello   world!  ";
+    printf("%s adr = s + 11: %d, expected 2\n", s4, word_len(s4 + 11));
+
+    char *s5 = "   Hello\t\tworld!  ";
+    printf("%s adr = s + 5: %d, expected 3\n", s5, word_len(s5 + 5));
+
+    char *s6 = " abc d";
+    printf("%s , adr = s + 1 : %d, expected 3\n", s6, word_len(s6 + 1));
+
+    puts("---------------- End Test word_len ----------------\n\n");
+}
 void test_extract_word() {}
 void test_next_word() {}
