b1d091dd4637e1d0ad8db14c46597a1f3522a537
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-03-17 09:01:45 +0100

    exo 1.1

--- TPs/TP7/Exercice1.c
+++ TPs/TP7/Exercice1.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 int nbr_words(const char *s);
 int word_len(const char *w);
@@ -22,8 +23,56 @@ int main()
     test_extract_word();
     test_next_word();
 }
+int nbr_words(const char *s)
+{
 
-void test_nbr_words() {}
+    int words_found = 0;
+    bool is_inside_word = false;
+
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (isspace(s[i]))
+        {
+            is_inside_word = false;
+        }
+        else
+        {
+            if (is_inside_word)
+            {
+                continue;
+            }
+
+            words_found++;
+            is_inside_word = true;
+        }
+    }
+
+    return words_found;
+}
+
+void test_nbr_words()
+{
+
+    puts("---------------- Test nbr_words ----------------");
+
+    char *s1 = "Hello world!";
+    printf("%s : %d, expected 2\n", s1, nbr_words(s1));
+
+    char *s2 = "  Hello world!";
+    printf("%s : %d, expected 2\n", s2, nbr_words(s2));
+
+    char *s3 = "Hello world!  ";
+    printf("%s : %d, expected 2\n", s3, nbr_words(s3));
+
+    char *s4 = "   Hello   world!  ";
+    printf("%s : %d, expected 2\n", s4, nbr_words(s4));
+
+    char *s5 = "   Hello\t\tworld!  ";
+    printf("%s : %d, expected 2\n", s5, nbr_words(s5));
+
+    char *s6 = " a aa ba a bbbb ";
+    printf("%s : %d, expected 5\n", s6, nbr_words(s6));
+}
 void test_word_len() {}
 void test_extract_word() {}
 void test_next_word() {}
