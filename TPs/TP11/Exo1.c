28d0c9d7394b2f4f6fab527e6f21c35213595495
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-04-20 14:16:24 +0200

    Exo 1.3

--- TPs/TP11/Exo1.c
+++ TPs/TP11/Exo1.c
@@ -1,8 +1,16 @@
+#include <stdio.h>
+
 int main(){
     int a = 69;
 
     void * pt = &a;
+    printf("a = %d\n",a);
+    *(int*)pt = 42; 
+    printf("a = %d\n",a);
+    *(int*)pt = *((int*)pt) * *(int*)pt; 
+    printf("a = %d\n",a);
 
     
-
 }
+
+
