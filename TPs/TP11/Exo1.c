929445ffa800eaf2673ec93a81bba3fd3b63303b
Yago Iglesias <yago.iglesias-vazquez@etu.u-paris.fr>
2023-04-20 14:20:24 +0200

    Exo 1.4

--- TPs/TP11/Exo1.c
+++ TPs/TP11/Exo1.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-int main(){
+typedef struct
+{
+    int x, y;
+} paire;
+
+int main()
+{
     int a = 69;
 
-    void * pt = &a;
-    printf("a = %d\n",a);
-    *(int*)pt = 42; 
-    printf("a = %d\n",a);
-    *(int*)pt = *((int*)pt) * *(int*)pt; 
-    printf("a = %d\n",a);
-
-    
+    void *pt = &a;
+    printf("a = %d\n", a);
+    *(int *)pt = 42;
+    printf("a = %d\n", a);
+    *(int *)pt = *((int *)pt) * *(int *)pt;
+    printf("a = %d\n", a);
 }
-
-
